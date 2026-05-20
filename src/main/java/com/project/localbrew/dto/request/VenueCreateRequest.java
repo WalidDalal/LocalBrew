6f13600ff066bc936b2df0057390cd6771e0f4ad
Gabriel Roncolato <gabrielroncolato0007@gmail.com>
2026-05-20 16:45:30 +0200

    Modifica su del di VenueCreateRequest

--- src/main/java/com/project/localbrew/dto/request/VenueCreateRequest.java
+++ src/main/java/com/project/localbrew/dto/request/VenueCreateRequest.java
@@ -2,10 +2,10 @@ package com.project.localbrew.dto.request;
 
 import com.project.localbrew.entity.VenueStatus;
 import com.project.localbrew.entity.VenueType;
-import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotNull;
-import jakarta.validation.constraints.Size;
+import jakarta.validation.constraints.*;
 import lombok.*;
+
+import java.util.UUID;
 
 @Getter
 @Setter
@@ -13,26 +13,33 @@ import lombok.*;
 @AllArgsConstructor
 @Builder
 public class VenueCreateRequest {
-    @NotBlank
-    @Size(max = 50)
+    @NotBlank(message = "Il nome è obbligatorio")
+    @Size(max = 50, message = "Il nome non può superare 50 caratteri")
     private String name;
 
-    @Size(max = 500)
+    @Size(max = 500, message = "La descrizione non può superare 500 caratteri")
     private String description;
 
-    @NotBlank
-    @Size(max = 70)
+    @NotBlank(message = "L'indirizzo è obbligatorio")
+    @Size(max = 70, message = "L'indirizzo non può superare 70 caratteri")
     private String address;
 
-    @NotNull
+    @NotNull(message = "La latitudine è obbligatoria")
+    @DecimalMin(value = "-90.0", message = "Latitudine minima -90")
+    @DecimalMax(value = "90.0", message = "Latitudine massima 90")
     private Double latitude;
 
-    @NotNull
+    @NotNull(message = "La longitudine è obbligatoria")
+    @DecimalMin(value = "-180.0", message = "Longitudine minima -180")
+    @DecimalMax(value = "180.0", message = "Longitudine massima 180")
     private Double longitude;
 
-    @NotNull
+    @NotNull(message = "Il tipo è obbligatorio")
     private VenueType type;
 
-    @NotNull
+    @NotNull(message = "Il tipo è obbligatorio")
     private VenueStatus status;
+
+    @NotNull(message = "Owner ID obbligatorio")
+    private UUID ownerId;
 }

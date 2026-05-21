757c15d9104a5e1f683b7810a48c000d403bb446
evamicadeva <229135266+evamicadeva@users.noreply.github.com>
2026-05-21 10:32:34 +0200

    Update VenueServiceImpl.java
    Aggiunta annotazione @ Service mancante

--- src/main/java/com/project/localbrew/service/VenueServiceImpl.java
+++ src/main/java/com/project/localbrew/service/VenueServiceImpl.java
@@ -13,6 +13,7 @@ import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 
+@Service
 public class VenueServiceImpl implements VenueService {
     private final VenueRepository venueRepository;
     private final UserRepository userRepository;

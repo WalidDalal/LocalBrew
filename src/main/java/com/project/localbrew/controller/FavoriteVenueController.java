806ed8b9783efd5a2c0fbe899e263359d6ba1c40
Gabriel Roncolato <gabrielroncolato0007@gmail.com>
2026-05-27 17:38:31 +0200

    Modifica di FavoriteVenueController

--- src/main/java/com/project/localbrew/controller/FavoriteVenueController.java
+++ src/main/java/com/project/localbrew/controller/FavoriteVenueController.java
@@ -7,11 +7,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/api/V1")
 public class FavoriteVenueController {
-    private final FavoriteDrinkService favoriteDrinkService;
 
-    public FavoriteVenueController(FavoriteDrinkService favoriteDrinkService) {
-        this.favoriteDrinkService = favoriteDrinkService;
-    }
 
 
 }

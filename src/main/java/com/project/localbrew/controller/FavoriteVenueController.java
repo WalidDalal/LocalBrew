064ec437fd8d14c13260c8affe84dbc392c028f5
Gabriel Roncolato <gabrielroncolato0007@gmail.com>
2026-05-27 17:15:15 +0200

    Creazione di FavoriteVenueController

---
+++ src/main/java/com/project/localbrew/controller/FavoriteVenueController.java
@@ -0,0 +1,10 @@
+package com.project.localbrew.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/V1")
+public class FavoriteVenueController {
+
+}

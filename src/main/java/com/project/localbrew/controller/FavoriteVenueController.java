9dd0bdb70132b6ba5a307ec236e6d43877770351
Gabriel Roncolato <gabrielroncolato0007@gmail.com>
2026-05-27 17:25:29 +0200

    Iniezione Service su FavoriteVenueController di FavoriteDrinkService

--- src/main/java/com/project/localbrew/controller/FavoriteVenueController.java
+++ src/main/java/com/project/localbrew/controller/FavoriteVenueController.java
@@ -1,10 +1,17 @@
 package com.project.localbrew.controller;
 
+import com.project.localbrew.service.FavoriteDrinkService;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api/V1")
 public class FavoriteVenueController {
+    private final FavoriteDrinkService favoriteDrinkService;
+
+    public FavoriteVenueController(FavoriteDrinkService favoriteDrinkService) {
+        this.favoriteDrinkService = favoriteDrinkService;
+    }
+
 
 }

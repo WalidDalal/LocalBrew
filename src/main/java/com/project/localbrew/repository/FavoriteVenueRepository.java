139260284b48512d5f30e210ecd83c1430863b0c
Gabriel Roncolato <gabrielroncolato0007@gmail.com>
2026-05-28 09:38:52 +0200

    Aggiunti nuovi metodi di ricerca in FavoriteVenueRepository

--- src/main/java/com/project/localbrew/repository/FavoriteVenueRepository.java
+++ src/main/java/com/project/localbrew/repository/FavoriteVenueRepository.java
@@ -1,5 +1,7 @@
 package com.project.localbrew.repository;
 
+import java.util.List;
+import java.util.Optional;
 import java.util.UUID;
 
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -9,5 +11,7 @@ import com.project.localbrew.entity.FavoriteVenue;
 
 @Repository
 public interface FavoriteVenueRepository extends JpaRepository<FavoriteVenue, UUID> {
-
+    boolean existsByUser_idAndVenue_id(UUID userId, UUID venueId);
+    List<FavoriteVenue> findAllByUser_id(UUID userId);
+    Optional<FavoriteVenue> findByUser_idAndVenue_id(UUID userId, UUID venueId);
 }

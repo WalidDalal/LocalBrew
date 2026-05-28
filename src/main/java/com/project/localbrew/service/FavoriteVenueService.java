1cf55dc0ae389e9262511022ea44ca611fa3fa56
Gabriel Roncolato <gabrielroncolato0007@gmail.com>
2026-05-28 09:48:39 +0200

    Aggiunti nuovi metodi in FavoriteVenueService

--- src/main/java/com/project/localbrew/service/FavoriteVenueService.java
+++ src/main/java/com/project/localbrew/service/FavoriteVenueService.java
@@ -1,4 +1,13 @@
 package com.project.localbrew.service;
 
+import com.project.localbrew.entity.FavoriteVenue;
+
+import java.util.List;
+import java.util.UUID;
+
 public interface FavoriteVenueService {
+    FavoriteVenue saveFavoriteVenue(UUID venueId);
+    List<FavoriteVenue> findByFavoriteVenues();
+    FavoriteVenue findByFavoriteVenuesId(UUID id);
+    void deleteFavoriteVenueByVenueId(UUID venueId);
 }

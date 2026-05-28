67b59c5df1a841fbbed600e72d7c46df8e2cf526
Gabriel Roncolato <gabrielroncolato0007@gmail.com>
2026-05-28 10:58:31 +0200

    Aggiunti nuovi metodi in FavoriteVenueController

--- src/main/java/com/project/localbrew/controller/FavoriteVenueController.java
+++ src/main/java/com/project/localbrew/controller/FavoriteVenueController.java
@@ -1,13 +1,50 @@
 package com.project.localbrew.controller;
 
-import com.project.localbrew.service.FavoriteDrinkService;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.project.localbrew.dto.response.FavoriteVenueResponse;
+import com.project.localbrew.entity.FavoriteVenue;
+import com.project.localbrew.entity.Venue;
+import com.project.localbrew.service.FavoriteVenueService;
+import jakarta.validation.constraints.NotNull;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.UUID;
 
 @RestController
-@RequestMapping("/api/V1")
+@RequestMapping("/api/v1/user/favotite-venues")
 public class FavoriteVenueController {
+    private final FavoriteVenueService favoriteVenueService;
 
+    public FavoriteVenueController(FavoriteVenueService favoriteVenueService) {
+        this.favoriteVenueService = favoriteVenueService;
+    }
 
+    @PostMapping("/{venueId}")
+    public ResponseEntity<FavoriteVenueResponse> addFavoriteVenue(@PathVariable @NotNull(message = "Venue id non può essere nullo") UUID venueId){
+        FavoriteVenue favoriteVenue = favoriteVenueService.saveFavoriteVenue(venueId);
 
+        return  ResponseEntity.ok(toResponse(favoriteVenue));
+    }
+
+    @GetMapping
+    public ResponseEntity<List<FavoriteVenueResponse>> findMyFavoriteVenues(){
+        List<FavoriteVenueResponse> favoriteVenues = favoriteVenueService.findByFavoriteVenues()
+                .stream()
+                .map(this::toResponse)
+                .toList();
+
+        return ResponseEntity.ok(favoriteVenues);
+    }
+
+    private FavoriteVenueResponse toResponse(FavoriteVenue favoriteVenue) {
+        Venue venue = favoriteVenue.getVenue();
+
+        return FavoriteVenueResponse.builder()
+                .id(favoriteVenue.getId())
+                .venueName(venue.getName())
+                .address(venue.getAddress())
+                .venueType(venue.getType())
+                .build();
+    }
 }

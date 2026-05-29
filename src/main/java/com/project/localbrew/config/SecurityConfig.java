a0717eab5aa0653ecf5477e890a5eb50acc14169
evamicadeva <229135266+evamicadeva@users.noreply.github.com>
2026-05-29 14:05:57 +0200

    Add additional allowed origins for CORS

--- src/main/java/com/project/localbrew/config/SecurityConfig.java
+++ src/main/java/com/project/localbrew/config/SecurityConfig.java
@@ -41,9 +41,11 @@ public class SecurityConfig {
         CorsConfiguration configuration = new CorsConfiguration();
 
         configuration.setAllowedOrigins(List.of(
-                "http://localhost:8080",
-                "http://127.0.0.1:8080"
-        ));
+        "http://localhost:8080",
+        "http://127.0.0.1:8080",
+        "http://localhost:5500",
+        "http://127.0.0.1:5500"
+));
 
         configuration.setAllowedMethods(List.of(
                 "GET",
